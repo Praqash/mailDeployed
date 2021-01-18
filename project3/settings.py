3dd23a91d2c7857ebcb3ac8efe9d83293a6589a0
Praqash <63649681+Praqash@users.noreply.github.com>
2021-01-18 22:52:55 +0530

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -130,11 +130,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
-EMAIL_HOST_PASSWORD = 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
+EMAIL_HOST_PASSWORD =  SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
