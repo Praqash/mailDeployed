20f5ec4a190e9e3d1684f4361d5fdfa2cc325c4e
Praqash <63649681+Praqash@users.noreply.github.com>
2021-01-21 13:15:28 +0530

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -49,10 +49,11 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
