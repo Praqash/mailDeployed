8fb3b1ca477d79b2252774c715a1bcef5c1c0038
Praqash <63649681+Praqash@users.noreply.github.com>
2021-01-22 05:18:21 +0530

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['praqash.info']
+ALLOWED_HOSTS = ['coronamail.herokuapp.com', 'https://praqash.info']
 
 
 # Application definition
